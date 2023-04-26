77c864f783e4de73c6ab4aff617558ce35c0ca15
George Lucas Gomes <gomes.georgelucas@gmail.com>
2023-04-26 19:35:21 -0400

    feat: add in memory user and passwd management

---
+++ app/src/main/java/br/edu/icomp/plaintext/PasswordDAO.java
@@ -0,0 +1,50 @@
+package br.edu.icomp.plaintext;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class PasswordDAO {
+
+    private Context context;
+    private static ArrayList<Password> passwordsList = new ArrayList<>();
+
+    public PasswordDAO(Context context) {
+        this.context = context;
+    }
+
+    public ArrayList<Password> getList() {
+        if (passwordsList.size() == 0) {
+            passwordsList.add(new Password(0, "Facebook", "dovahkiin@gmail.com",
+                    "FusRoDah123", ""));
+            passwordsList.add(new Password(1, "GMail", "dovahkiin",
+                    "Teste123", ""));
+            passwordsList.add(new Password(2, "IComp", "dfrd@icomp.ufam.edu.br",
+                    "Java4242", "Mudar a senha!"));
+            passwordsList.add(new Password(3, "Steam", "dovahkiin",
+                    "FusRoDah123", "Conta do Brasil"));
+        }
+        return passwordsList;
+    }
+
+    public boolean add(Password password) {
+        password.setId(passwordsList.size());
+        passwordsList.add(password);
+        Toast.makeText(context, "Senha salva!", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    public boolean update(Password password) {
+        passwordsList.set(password.getId(), password);
+        Toast.makeText(context, "Senha atualizada!", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    public Password get(int id) {
+        return passwordsList.get(id);
+    }
+
+
+
+}
